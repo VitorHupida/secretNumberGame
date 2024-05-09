740ba022eecbb3e63c6ffe916b02f5b36ae7abd0
Vitor Hugo <vitorpierinidantas@hotmail.com>
2024-05-08 23:51:51 -0300

    Alterando limite de 100 para 50

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let quantidadeNumeroDisponivel = 100;
+let quantidadeNumeroDisponivel = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function verificarTextoNaTela(tag, texto){
 
 function exibirMensagemInicial() {
     verificarTextoNaTela("h1","Jogo do número secreto");
-    verificarTextoNaTela("p","Escolha um número entre 1 e 100");
+    verificarTextoNaTela("p","Escolha um número entre 1 e 50");
 }
 
 exibirMensagemInicial();
